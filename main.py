66597a47a9fbcc2822112e0037e931ccd5e6aa7f
Amanda Moore <amanda.joy24@outlook.com>
2022-08-01 13:37:13 -0700

    ping slack on boot

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import multiprocessing
 import time
 import updater
+import asyncio
+from itgs import Itgs
 
 
-def main():
+async def _main():
     multiprocessing.Process(target=updater.listen_forever_sync, daemon=True).start()
+    async with Itgs() as itgs:
+        slack = await itgs.slack()
+        await slack.send_web_error_message("hello from jobs")
     while True:
         print("Hello World")
         time.sleep(5)
+
+
+def main():
+    asyncio.run(_main())
 
 
 if __name__ == "__main__":
